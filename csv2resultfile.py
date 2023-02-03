43dcf35637e84b59d9b36744ab315687d9f2e80b
Dominik Köppl <git@dkppl.de>
2023-02-03 13:15:40 +0900

    csv2result with custom delimiter

--- csv2resultfile.py
+++ csv2resultfile.py
@@ -3,9 +3,28 @@
 
 import sys
 
-with open(sys.argv[1]) as csvfile:
-    header = [x.strip() for x in csvfile.readline().split(',')]
+import argparse
+
+import codecs
+
+def unescaped_str(arg_str):
+    return codecs.decode(str(arg_str), 'unicode_escape')
+
+parser = argparse.ArgumentParser(description='converts a CSV file into a RESULT file')
+parser.add_argument('-i', "--input", type=str, help="input CSV file", required=True)
+parser.add_argument('-o', "--output", type=str, help="output stats file", default='')
+parser.add_argument('-d', '--delimiter', type=unescaped_str, default=',')
+args = parser.parse_args()
+
+outfile = sys.stdout
+if args.output:
+    outfile = open(args.output)
+
+with open(args.input) as csvfile:
+    header = [x.strip() for x in csvfile.readline().split(args.delimiter)]
     for line in csvfile.readlines():
-        csvlist = line.split(',')
-        print("RESULT " + "\t".join(list(map(lambda x,y: (x + "=" + y), header, csvlist))))
+        csvlist = line.split(args.delimiter)
+        print("RESULT " + "\t".join(list(map(lambda x,y: (x + "=" + y.strip()), header, csvlist))), file=outfile)
+
+outfile.close()
 
